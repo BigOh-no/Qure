c33b28e8871addf935800376e7d5e59e7424932d
aaliahreddy <2798790@students.wits.ac.za>
2026-04-11 12:21:13 +0200

    Staff dashboard created

--- qure/src/App.js
+++ qure/src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import AdminDashboard from "./pages/Admin-dashboard"
 import AuthCallback from "./pages/AuthCallback";
+import StaffDashboard from "./pages/Staff-dashboard";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
+        <Route path="/staff" element={<StaffDashboard />} />
       </Routes>
     </Router>
   );
---
+++ qure/src/pages/Staff-dashboard.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const StaffDashboard = () => {
+  return <div>Welcome to the Staff Dashboard!</div>;
+};
+
+export default StaffDashboard;
