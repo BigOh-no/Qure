cb03f280e3b7e00ca7be852fe42ca61b528a9acf
Muhammed Bayat <bayatm2005@gmail.com>
2026-04-16 16:11:06 +0200

    fix deployment issue

--- qure/src/App.js
+++ qure/src/App.js
@@ -1,15 +1,4 @@
 import L from "leaflet";
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-});
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
@@ -22,6 +11,17 @@ import StaffDashboard from "./pages/Staff-dashboard";
 import BookAppointment from "./pages/BookAppointment";
 import QueuePage from "./pages/QueuePage";
 import AppointmentsPage from "./pages/AppointmentsPage";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 function App() {
   return (
