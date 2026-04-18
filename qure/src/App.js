a82f90cb846cb2a281dd095fba12210bdd990fd7
Vareshan <vareshan123@gmail.com>
2026-04-18 19:37:33 +0200

    Fixing merge

--- qure/src/App.js
+++ qure/src/App.js
@@ -8,26 +8,12 @@ import AdminDashboard from "./pages/Admin-dashboard";
 import PatientDashboard from "./pages/Patient-dashboard";
 import AuthCallback from "./pages/AuthCallback";
 import StaffDashboard from "./pages/Staff-dashboard";
-<<<<<<< HEAD
 import BookAppointment from "./pages/BookAppointment";
 import QueuePage from "./pages/QueuePage";
 import AppointmentsPage from "./pages/AppointmentsPage";
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-});
-=======
 import ResetPasswordPage from "./pages/resetPasswordPage";
 import StaffAuth from "./pages/staffAuth";
 import AdminAuth from "./pages/adminAuth";
->>>>>>> AddStaff
 
 function App() {
   return (
@@ -38,14 +24,11 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/patient" element={<PatientDashboard />} />
-<<<<<<< HEAD
+        <Route path="/staff" element={<StaffDashboard />} />
         <Route path="/patient/book" element={<BookAppointment />} />
         <Route path="/patient/queue" element={<QueuePage />} />
         <Route path="/patient/appointments" element={<AppointmentsPage />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
-=======
->>>>>>> AddStaff
-        <Route path="/staff" element={<StaffDashboard />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
         <Route path="/admin/auth/callback" element={<AdminAuth />} />
         <Route path="/staff/auth/callback" element={<StaffAuth />} />
